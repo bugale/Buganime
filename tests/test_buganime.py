fe7c107479c7f188ff2bc0e74e65c0d9eda6c118
Bugale Bugalit <bugalit@gmail.com>
2024-10-16 20:04:24 +0300

    fix: handle more filenames

--- tests/test_buganime.py
+++ tests/test_buganime.py
@@ -12,7 +12,7 @@ from buganime import buganime, transcode
 
 NAME_CONVERSIONS = [
     (r'C:\[SHiN-gx] Fight Ippatsu! Juuden-chan!! - Special 1 [720x480 AR h.264 FLAC][v2][FF09021F].mkv',
-     buganime.TVShow(name='Fight Ippatsu! Juuden-chan!!', season=0, episode=1)),
+     buganime.TVShow(name='Fight Ippatsu! Juuden chan!!', season=0, episode=1)),
 
     (r'C:\[gleam] Kurenai OVA - 01 [OAD][0e73f000].mkv',
      buganime.TVShow(name='Kurenai', season=0, episode=1)),
@@ -21,19 +21,19 @@ NAME_CONVERSIONS = [
      buganime.TVShow(name='Saki', season=0, episode=1)),
 
     (r'C:\[CoalGuys] K-ON!! S2 - 05 [4B19B10F].mkv',
-     buganime.TVShow(name='K-ON!!', season=2, episode=5)),
+     buganime.TVShow(name='K ON!!', season=2, episode=5)),
 
     (r'C:\[SubsPlease] RWBY - Hyousetsu Teikoku - 01 (1080p) [FA9C5B87].mkv',
-     buganime.TVShow(name='RWBY - Hyousetsu Teikoku', season=1, episode=1)),
+     buganime.TVShow(name='RWBY Hyousetsu Teikoku', season=1, episode=1)),
 
     (r'C:\[SubsPlease] Tokyo Mew Mew New - 01 (1080p) [440C0CD7].mkv',
      buganime.TVShow(name='Tokyo Mew Mew New', season=1, episode=1)),
 
     (r'C:\[Erai-raws] Shin Tennis no Ouji-sama - U-17 World Cup - 01 [1080p][Multiple Subtitle][0341CBE1].mkv',
-     buganime.TVShow(name='Shin Tennis no Ouji-sama - U-17 World Cup', season=1, episode=1)),
+     buganime.TVShow(name='Shin Tennis no Ouji sama U 17 World Cup', season=1, episode=1)),
 
     (r'C:\[Judas] Kaguya-Sama Wa Kokurasetai - S03E07.mkv',
-     buganime.TVShow(name='Kaguya-Sama Wa Kokurasetai', season=3, episode=7)),
+     buganime.TVShow(name='Kaguya Sama Wa Kokurasetai', season=3, episode=7)),
 
     (r'C:\[SubsPlease] Rikei ga Koi ni Ochita no de Shoumei shitemita - 08v2 (1080p) [77514EF3].mkv',
      buganime.TVShow(name='Rikei ga Koi ni Ochita no de Shoumei shitemita', season=1, episode=8)),
@@ -42,17 +42,17 @@ NAME_CONVERSIONS = [
      buganime.TVShow(name='Rikei ga Koi ni Ochita no de Shoumei shitemita', season=2, episode=8)),
 
     (r'C:\Kaguya-sama - Love is War - S00E01 - (S2O1 OVA).mkv',
-     buganime.TVShow(name='Kaguya-sama - Love is War', season=0, episode=1)),
+     buganime.TVShow(name='Kaguya sama Love is War', season=0, episode=1)),
 
     (r'C:\Kaguya-sama - Love is War - S01E06.mkv',
-     buganime.TVShow(name='Kaguya-sama - Love is War', season=1, episode=6)),
+     buganime.TVShow(name='Kaguya sama Love is War', season=1, episode=6)),
 
     (r'C:\Kaguya-sama wa Kokurasetai S03 1080p Dual Audio WEBRip AAC x265-EMBER\S03E01-Miko Iino Wants to Be Soothed Kaguya Doesn’t Realize Chika Fujiwara '
         r'Wants to Battle [8933E8C9].mkv',
-     buganime.TVShow(name='Kaguya-sama wa Kokurasetai', season=3, episode=1)),
+     buganime.TVShow(name='Kaguya sama wa Kokurasetai', season=3, episode=1)),
 
     (r'C:\Kaguya-sama wa Kokurasetai S2 - OVA - 1080p WEB H.264 -NanDesuKa (B-Global).mkv',
-     buganime.TVShow(name='Kaguya-sama wa Kokurasetai', season=0, episode=1)),
+     buganime.TVShow(name='Kaguya sama wa Kokurasetai', season=0, episode=1)),
 
     (r'C:\Tensei shitara Ken Deshita - 01 - 2160p WEB H.264 -NanDesKa.mkv',
      buganime.TVShow(name='Tensei shitara Ken Deshita', season=1, episode=1)),
@@ -77,6 +77,13 @@ NAME_CONVERSIONS = [
 
     (r'C:\Mushoku Tensei S02P01+SP 1080p Dual Audio BDRip 10 bits DD+ x265-EMBER\S02E01-The Brokenhearted Mage [AFBB9792].mkv',
      buganime.TVShow(name='Mushoku Tensei', season=2, episode=1)),
+
+    (r'C:\A.Terrified.Teacher.at.Ghoul.School!.S01E01.Welcome.to.Hyakki.Academy!.1080p.CR.WEB-DL.JPN.AAC2.0.H.264.MSubs-ToonsHub.mkv',
+     buganime.TVShow(name='A Terrified Teacher at Ghoul School!', season=1, episode=1)),
+
+    (r'C:\Tohai.-.Ura.Rate.Mahjong.Tohai.Roku.S01E01.1080p.AMZN.WEB-DL.DDP2.0.H.264-Emmid'
+        r'\Tohai.-.Ura.Rate.Mahjong.Tohai.Roku.S01E01.1080p.AMZN.WEB-DL.DDP2.0.H.264-Emmid.mkv',
+     buganime.TVShow(name='Tohai Ura Rate Mahjong Tohai Roku', season=1, episode=1)),
 ]
 
 
